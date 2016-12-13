3ab44f88bd4c13e39ce6af4bcea97d67e143b59c
Kevin Stessel <kevin.stessel@edu.fh-joanneum.at>
2016-12-13 15:31:11 +0100

    Triangle and subTriangle drawable

--- app/src/main/scala/at/fh/swengb/sierpinskitriangle/Sierpinski.scala
+++ app/src/main/scala/at/fh/swengb/sierpinskitriangle/Sierpinski.scala
@@ -17,25 +17,58 @@ class Sierpinski(val context: Context, val attrs: AttributeSet) extends View(con
   {
     super.onDraw(canvas)
     val paint=new Paint();
-    paint.setColor(Color.rgb(255, 153, 51));
+    //line color
+    paint.setColor(Color.rgb(5, 10, 0));
+    //line width
     paint.setStrokeWidth(10);
+    //x...line (gleichseitig)
+    val x = canvas.getWidth
+    val height = Math.sqrt(x * x - x/2 * x/2).toFloat
+
+    drawTriangle(canvas,0,10,x,10,x/2,height,paint)
+    subDrawTriangle(canvas,x/2,10,(x/2)/2,height/2,3*x/4,height/2,paint)
+    subDrawTriangle(canvas,(x/2)/2,10,((x/2)/2)/2,(height/2)/2,(3*x/4)/2,(height/2)/2,paint)
+
+    def drawTriangle(canvas: Canvas,px1: Float, py1: Float, px2: Float, py2: Float, px3:Float, py3:Float,paint: Paint)={
+      paint.setColor(Color.rgb(5, 10, 139));
+      canvas.drawLines(Array(px1,py1,px2,py2,px2,py2,px3,py3,px3,py3,px1,py1),paint)
+
+    }
+    def subDrawTriangle(canvas: Canvas,px1: Float, py1: Float, px2: Float, py2: Float, px3:Float, py3:Float,paint: Paint)= {
+      canvas.drawLines(Array(px1,py1,px2,py2,px2,py2,px3,py3,px3,py3,px1,py1),paint)
+    }
+  /* old version => hard coded - first triangle
     val startx = 0
     val starty = 10
-    val stopx = canvas.getWidth
+    val stopx = x
     val stopy = 10
-    canvas.drawLine(startx,starty,stopx,stopy,paint)
+    canvas.drawLine(startx,starty,x,10,paint)
     val startx1 = 0
     val starty1 = 10
     val stopx1 = canvas.getWidth/2
-    val stopy1 = Math.sqrt(stopx1 * stopx1 - stopx1/2 * stopx1/2).toFloat +10
+    val stopy1 = Math.sqrt(stopx * stopx - stopx/2 * stopx/2).toFloat +10
     canvas.drawLine(startx1,starty1,stopx1,stopy1,paint)
     val startx2 = canvas.getWidth
     val starty2 = 0+10
     val stopx2 = canvas.getWidth/2
-    val stopy2 = Math.sqrt(stopx1 * stopx1 - stopx1/2 * stopx1/2).toFloat +10
+    val stopy2 = Math.sqrt(stopx * stopx - stopx/2 * stopx/2).toFloat +10
+    canvas.drawLine(startx2,starty2,stopx2,stopy2,paint)*/
+    /*
+    first subTriangle
+    val startx = x/2
+    val starty = 10
+    val stopx = (x/2)/2
+    val stopy = height/2
+    canvas.drawLine(startx,starty,stopx,stopy,paint)
+    val startx2 = (x/2)/2
+    val starty2 = height/2
+    val stopx2= 3*x/4
+    val stopy2 = height/2
     canvas.drawLine(startx2,starty2,stopx2,stopy2,paint)
+    val startx1 = x/2
+    val starty1 = 10
+    val stopx1 = 3*x/4
+    val stopy1 = height/2
+    canvas.drawLine(startx1,starty1,stopx1,stopy1,paint)*/
   }
-
-
-
 }
