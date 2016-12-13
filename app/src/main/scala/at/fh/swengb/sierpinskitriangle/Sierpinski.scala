9cd43cce0bca3893a1cb3c2f2b4dfa30e57a603a
kiv1995 <kevin.stessel@edu.fh-joanneum.at>
2016-12-13 12:01:00 +0100

    Draw a Triangle

--- app/src/main/scala/at/fh/swengb/sierpinskitriangle/Sierpinski.scala
+++ app/src/main/scala/at/fh/swengb/sierpinskitriangle/Sierpinski.scala
@@ -1,19 +1,42 @@
 package at.fh.swengb.sierpinskitriangle
 
 import android.content.Context
-import android.graphics.{Canvas, Paint, Path}
+import android.graphics.{Canvas, Color, Paint, Path}
+import android.util.AttributeSet
 import android.view.View
 
 /**
   * Created by Kevin on 13.12.2016.
   */
-class Sierpinski(val context: Context) extends View(context)
+class Sierpinski(val context: Context, val attrs: AttributeSet) extends View(context,attrs)
 {
-  override protected def onDraw(canvas: Canvas): Unit = {
-    super.onDraw(canvas)
-    val x = canvas.getWidth
-    val y = Math.sqrt(x * x - x / 2 * x / 2).toFloat
+  val paint = new Paint()
+  val depth = 7
 
+  override protected def onDraw(canvas: Canvas)
+  {
+    super.onDraw(canvas)
+    val paint=new Paint();
+    paint.setColor(Color.rgb(255, 153, 51));
+    paint.setStrokeWidth(10);
+    val startx = 0
+    val starty = 10
+    val stopx = canvas.getWidth
+    val stopy = 10
+    canvas.drawLine(startx,starty,stopx,stopy,paint)
+    val startx1 = 0
+    val starty1 = 10
+    val stopx1 = canvas.getWidth/2
+    val stopy1 = Math.sqrt(stopx1 * stopx1 - stopx1/2 * stopx1/2).toFloat +10
+    canvas.drawLine(startx1,starty1,stopx1,stopy1,paint)
+    val startx2 = canvas.getWidth
+    val starty2 = 0+10
+    val stopx2 = canvas.getWidth/2
+    val stopy2 = Math.sqrt(stopx1 * stopx1 - stopx1/2 * stopx1/2).toFloat +10
+    canvas.drawLine(startx2,starty2,stopx2,stopy2,paint)
+    canvas.drawLi
   }
+
+
 
 }
